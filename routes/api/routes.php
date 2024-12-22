8d5d5ff4cf9f40119d332973b86c0d5d457559b1
Adam Eastwood <adam-3@live.co.uk>
2024-12-22 02:33:10 +0000

    fixed routes

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::domain(config('app.domains.admin_url'))->middleware('api')->group(function () {
-    Route::get('/', function () {
-        return response()->json([
-            'message' => 'just doing api stuff idk :/'
-        ]);
-    });
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'just doing api stuff idk :/'
+    ]);
 });
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::domain(config('app.domains.api_url'))->group(function () {
-    dd('api');
-    require_once 'api/routes.php';
+    require_once base_path('routes/api/routes.php');
 });
 
 Route::domain(config('app.domains.admin_url'))->group(function () {
-    dd('admin');
-    require_once 'admin/routes.php';
+    require_once base_path('routes/admin/routes.php');
 });
+

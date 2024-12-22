1b6455e37450e2500cdebcfe1dd7c9468b65ec81
Adam Eastwood <adam-3@live.co.uk>
2024-12-22 02:36:17 +0000

    feat: force JSON for api

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Middleware\API;
 use Illuminate\Support\Facades\Route;
 
-Route::domain(config('app.domains.api_url'))->group(function () {
+Route::domain(config('app.domains.api_url'))->middleware(API::class)->group(function () {
     require_once base_path('routes/api/routes.php');
 });
 

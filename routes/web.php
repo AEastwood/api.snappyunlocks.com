1a2ca372c6c2dc5a09a71e4fc1281c5df76c9aa7
Adam Eastwood <adam-3@live.co.uk>
2024-12-22 02:59:31 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Middleware\API;
 use Illuminate\Support\Facades\Route;
 
 Route::domain(config('app.domains.api_url'))->middleware(API::class)->group(function () {
-    include_once base_path('routes/api/routes.php');
+    require_once base_path('routes/api/routes.php');
 });
 
 Route::domain(config('app.domains.admin_url'))->group(function () {
-    include_once base_path('routes/admin/routes.php');
+    require_once base_path('routes/admin/routes.php');
 });
